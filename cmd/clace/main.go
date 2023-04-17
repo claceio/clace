9ed779533c90e5d0cf476a24bcbbfc0ec6562164
Ajay Kidave <ajayvk@clace.io>
2023-04-16 18:48:36 -0700

    Added cmd

---
+++ cmd/clace/main.go
@@ -0,0 +1,10 @@
+// Copyright (c) Clace Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import "github.com/claceio/cmd/clace/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ cmd/claced/main.go
@@ -0,0 +1,10 @@
+// Copyright (c) Clace Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import "github.com/claceio/cmd/claced/cmd"
+
+func main() {
+	cmd.Execute()
+}
