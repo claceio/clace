b59085efcd3344b5e526d11ff8e816a0ebce7e64
Ajay Kidave <ajayvk@clace.io>
2023-04-20 16:18:20 -0700

    Changed to use urfave/cli/v2 for CLI
    $ clace
    NAME:
       clace - Clace client and server https://clace.io/
    
    USAGE:
       clace [global options] command [command options] [arguments...]
    
    COMMANDS:
       server
       help, h  Shows a list of commands or help for one command
    
    GLOBAL OPTIONS:
       --config_file value, -c value  TOML configuration file [$CL_CONFIG_FILE]
       --help, -h                     show help
    
    $ clace server start
    NAME:
       clace server start - Start the clace server
    
    USAGE:
       clace server start [command options] [arguments...]
    
    OPTIONS:
       --listen_host value, -i value  The interface to listen on (default: "127.0.01") [$CL_LISTEN_HOST]
       --listen_port value, -p value  The port to listen on (default: 25223) [$CL_LISTEN_PORT]
       --log_level value, -l value    The logging level to use (default: "INFO") [$CL_LOG_LEVEL]
       --help, -h                     show help

--- cmd/clace/main.go
+++ cmd/clace/main.go
@@ -1,10 +1,132 @@
-// Copyright (c) Clace Inc
-// SPDX-License-Identifier: Apache-2.0
-
 package main
 
-import "clace.io/cmd/clace/cmd"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
+)
+
+const configFileFlagName = "config_file"
+
+type GlobalConfig struct {
+	ConfigFile string
+}
+
+type ServerConfig struct {
+	Host     string
+	Port     int
+	LogLevel string
+}
+
+type ClientConfig struct {
+}
+
+func allCommands(serverConfig *ServerConfig, clientConfig *ClientConfig) []*cli.Command {
+	var commands []*cli.Command
+	for _, v := range [][]*cli.Command{
+		serverCommands(serverConfig),
+		clientCommands(clientConfig),
+	} {
+		commands = append(commands, v...)
+	}
+	return commands
+}
+
+func clientCommands(clientConfig *ClientConfig) []*cli.Command {
+	return nil
+}
+
+func newStringFlag(name, alias, usage, value string, destionation *string) *altsrc.StringFlag {
+	envString := fmt.Sprintf("CL_%s", strings.ToUpper(name))
+	var aliases []string
+	if alias != "" {
+		aliases = []string{alias}
+	}
+	return altsrc.NewStringFlag(&cli.StringFlag{
+		Name:        name,
+		Aliases:     aliases,
+		Usage:       usage,
+		Value:       value,
+		EnvVars:     []string{envString},
+		Destination: destionation,
+	})
+}
+
+func newIntFlag(name, alias, usage string, value int, destionation *int) *altsrc.IntFlag {
+	envString := fmt.Sprintf("CL_%s", strings.ToUpper(name))
+	var aliases []string
+	if alias != "" {
+		aliases = []string{alias}
+	}
+	return altsrc.NewIntFlag(&cli.IntFlag{
+		Name:        name,
+		Aliases:     aliases,
+		Usage:       usage,
+		Value:       value,
+		EnvVars:     []string{envString},
+		Destination: destionation,
+	})
+}
+
+func serverCommands(serverConfig *ServerConfig) []*cli.Command {
+	flags := []cli.Flag{
+		newStringFlag("listen_host", "i", "The interface to listen on", "127.0.01", &serverConfig.Host),
+		newIntFlag("listen_port", "p", "The port to listen on", 25223, &serverConfig.Port),
+		newStringFlag("log_level", "l", "The logging level to use", "INFO", &serverConfig.LogLevel),
+	}
+
+	return []*cli.Command{
+		{
+			Name: "server",
+			Subcommands: []*cli.Command{
+				{
+					Name:   "start",
+					Usage:  "Start the clace server",
+					Flags:  flags,
+					Before: altsrc.InitInputSourceWithContext(flags, altsrc.NewTomlSourceFromFlagFunc(configFileFlagName)),
+					Action: func(cCtx *cli.Context) error {
+						fmt.Printf("Starting server, addr %s port %d log_level  %s\n",
+							serverConfig.Host, serverConfig.Port, serverConfig.LogLevel)
+						return nil
+					},
+				},
+			},
+		},
+	}
+}
+
+func globalFlags(globalConfig *GlobalConfig) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        configFileFlagName,
+			Aliases:     []string{"c"},
+			Usage:       "TOML configuration file",
+			Destination: &globalConfig.ConfigFile,
+			EnvVars:     []string{"CL_CONFIG_FILE"},
+		},
+	}
+}
 
 func main() {
-	cmd.Execute()
+	globalConfig := GlobalConfig{}
+	serverConfig := ServerConfig{}
+	clientConfig := ClientConfig{}
+	globalFlags := globalFlags(&globalConfig)
+
+	app := &cli.App{
+		Name:                 "clace",
+		Usage:                "Clace client and server https://clace.io/",
+		EnableBashCompletion: true,
+		Suggest:              true,
+		Flags:                globalFlags,
+		Before:               altsrc.InitInputSourceWithContext(globalFlags, altsrc.NewTomlSourceFromFlagFunc(configFileFlagName)),
+		Commands:             allCommands(&serverConfig, &clientConfig),
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println("error: ", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,25 +5,12 @@ module clace.io
 
 go 1.19
 
-require (
-	github.com/spf13/cobra v1.7.0
-	github.com/spf13/viper v1.15.0
-)
+require github.com/urfave/cli/v2 v2.25.1
 
 require (
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/spf13/afero v1.9.3 // indirect
-	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
