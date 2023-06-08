a43a79aae676948af3437b49e26e0a6f1ef467e3
Ajay Kidave <ajayvk@clace.io>
2023-06-07 21:51:24 -0700

    Added test for app creation and auto-sync

--- tests/run_cli_tests.sh
+++ tests/run_cli_tests.sh
@@ -8,6 +8,25 @@ rm -rf clace.db
 export CL_HOME=.
 unset CL_CONFIG_FILE
 
+trap "error_handler" ERR
+
+error_handler () {
+    echo "Error occurred, running cleanup"
+    cleanup
+    echo "Test failed"
+    exit 1
+}
+
+cleanup() {
+  rm -rf clace.db
+  #rm -rf logs/ clace.toml  server.stdout
+  set +e
+  ps -ax | grep "clace server start" | grep -v grep | cut -c1-6 | xargs kill -9
+
+  # Github Actions does not seem to allow kill, the last echo is to allow the exit code to be zero
+  echo "Done with cleanup"
+}
+
 # Test error messages
 ../clace server start --admin_password=abcd --http.port=9999 &
 sleep 2
@@ -27,11 +46,8 @@ export CL_CONFIG_FILE=clace.toml
 ../clace server start &
 sleep 2
 commander test --dir ./commander/
-rm -rf clace.db
-rm -rf logs/ clace.toml  server.stdout
 
-set +e
-ps -ax | grep "clace server start" | grep -v grep | cut -c1-6 | xargs kill -9
+echo $?
 
-# Github Actions does not seem to allow kill, the last echo is to allow the exit code to be zero
-echo "CLI tests succeeded"
+cleanup
+echo "All tests passed"
