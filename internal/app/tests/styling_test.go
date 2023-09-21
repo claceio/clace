449f03d2a7dd63189f6295d5799820061286f9aa
Ajay Kidave <ajayvk@clace.io>
2023-09-21 10:52:18 -0700

    Include custom style sheet if present

--- internal/app/tests/styling_test.go
+++ internal/app/tests/styling_test.go
@@ -129,6 +129,30 @@ app = clace.app("testApp", custom_layout=True, pages = [clace.page("/")],
 	testutil.AssertStringMatch(t, "tailwind.config.js", `module.exports = { content: ['*.go.html'], theme: { extend: {}, }, plugins: [ require("daisyui") ], daisyui: { themes: ["dark", "cupcake"], }, }`, string(data))
 }
 
+func TestStyleCustom(t *testing.T) {
+	logger := testutil.TestLogger()
+	fileData := map[string]string{
+		"app.star": `
+app = clace.app("testApp", custom_layout=False, pages = [clace.page("/")])`,
+		"static/css/style.css": "body { background-color: red; }",
+		"app.go.html":          `{{block "clace_body" .}}ABC{{end}}`,
+	}
+
+	a, _, err := app.CreateDevModeTestApp(logger, fileData)
+	if err != nil {
+		t.Fatalf("Error %s", err)
+	}
+
+	request := httptest.NewRequest("GET", "/test", nil)
+	response := httptest.NewRecorder()
+	a.ServeHTTP(response, request)
+
+	testutil.AssertEqualsInt(t, "code", 200, response.Code)
+	// Since custom style static/css/style.css is present, that should be included in the header
+	testutil.AssertStringContains(t, response.Body.String(),
+		`<link rel="stylesheet" href=/test/static/css/style-ac05e05bbc5e5410e5c9e7531bbd20c45803d479bb10e5a6e9d3c61d40e3e811.css>`)
+}
+
 func TestStyleError(t *testing.T) {
 	logger := testutil.TestLogger()
 	fileData := map[string]string{
