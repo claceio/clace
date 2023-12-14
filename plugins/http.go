d62cbbd0429fb6f254be145c11d98af833689887
Ajay Kidave <ajayvk@clace.io>
2023-12-13 17:27:12 -0800

    Added post handler

--- plugins/http.go
+++ plugins/http.go
@@ -41,6 +41,7 @@ func init() {
 		app.CreatePluginApi("get", true, h.reqMethod("get")),
 		app.CreatePluginApi("head", true, h.reqMethod("head")),
 		app.CreatePluginApi("options", true, h.reqMethod("options")),
+		app.CreatePluginApi("post", false, h.reqMethod("post")),
 		app.CreatePluginApi("put", false, h.reqMethod("put")),
 		app.CreatePluginApi("delete", false, h.reqMethod("delete")),
 		app.CreatePluginApi("patch", false, h.reqMethod("patch")),
