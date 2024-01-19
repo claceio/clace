1d628f9992cf6afc74ffb4bdd04b00fb5bb9edc4
Ajay Kidave <ajayvk@clace.io>
2024-01-19 10:01:46 -0800

    Added test file

---
+++ internal/app/tests/store_test.go
@@ -0,0 +1,108 @@
+// Copyright (c) ClaceIO, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package app_test
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/claceio/clace/internal/testutil"
+	"github.com/claceio/clace/internal/utils"
+)
+
+func TestStoreBasics(t *testing.T) {
+	logger := testutil.TestLogger()
+	fileData := map[string]string{
+		"app.star": `
+load("store.in", "store")
+
+app = ace.app("testApp", custom_layout=True, pages = [ace.page("/", type="json")],
+permissions=[
+	ace.permission("store.in", "insert"),
+	ace.permission("store.in", "select_by_id"),
+	ace.permission("store.in", "update"),
+	ace.permission("store.in", "delete_by_id"),
+]
+)
+
+def handler(req):
+	myt = star.test1(aint=10, astring="abc", abool=False, alist=[1], adict={'a': 1})
+	ret = store.insert(table.test1, myt)
+	if not ret:
+		return {"error": ret.Error}
+
+	id = ret.Data
+	ret = store.select_by_id(table.test1, id)
+	if not ret:
+		return {"error": ret.Error}
+
+	f = ret.Data
+	f.aint = 100
+	f.astring = "xyz"
+
+	upd_status = store.update(table.test1, f)
+	if not upd_status:
+		return {"error": ret.Error}
+
+	# Duplicate updates should fail (optimistic locking)
+	upd_status = store.update(table.test1, f)
+	if upd_status:
+		return {"error": "Expected duplicate update to fail"}
+
+	ret = store.select_by_id(table.test1, id)
+
+	del_status = store.delete_by_id(table.test1, id)
+	if not del_status:
+		return {"error": ret.Error}
+	del_status = store.delete_by_id(table.test1, id)
+	if del_status:
+		return {"error": "Expected delete to fail"}
+
+	return {"intval": ret.Data.aint, "stringval": ret.Data.astring,
+		"_id": ret.Data._id,
+		"creator": ret.Data._created_by, "created_at": ret.Data._created_at}
+	`,
+
+		"schema.star": `
+type("test1", fields=[
+    field("aint", INT),
+    field("astring", STRING),
+    field("abool", BOOLEAN),
+    field("alist", LIST),
+    field("adict", DICT),
+])`,
+		"index.go.html": ``,
+	}
+
+	a, _, err := CreateTestAppPlugin(logger, fileData, []string{"store.in"},
+		[]utils.Permission{
+			{Plugin: "store.in", Method: "insert"},
+			{Plugin: "store.in", Method: "select_by_id"},
+			{Plugin: "store.in", Method: "update"},
+			{Plugin: "store.in", Method: "delete_by_id"},
+		}, map[string]utils.PluginSettings{
+			"store.in": {
+				"db_connection": "sqlite:$CL_HOME/clace_app.db?_journal_mode=WAL",
+			},
+		})
+	if err != nil {
+		t.Fatalf("Error %s", err)
+	}
+
+	request := httptest.NewRequest("GET", "/test", nil)
+	response := httptest.NewRecorder()
+	a.ServeHTTP(response, request)
+
+	testutil.AssertEqualsInt(t, "code", 200, response.Code)
+	ret := make(map[string]any)
+	json.NewDecoder(response.Body).Decode(&ret)
+
+	testutil.AssertEqualsString(t, "creator", "admin", ret["creator"].(string))
+	testutil.AssertEqualsString(t, "astring", "xyz", ret["stringval"].(string))
+	id := ret["_id"].(float64)
+	if id <= 0 {
+		t.Errorf("Expected _id to be > 0, got %f", id)
+	}
+}
