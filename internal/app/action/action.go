edfda8f76c8ab227caee0d566ecded1ae5944ee1
Ajay Kidave <ajayvk@clace.io>
2024-10-08 20:52:21 -0700

    Added actions code

---
+++ internal/app/action/action.go
@@ -0,0 +1,94 @@
+// Copyright (c) ClaceIO, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package action
+
+import (
+	"embed"
+	"html/template"
+	"io/fs"
+	"net/http"
+	"path"
+	"slices"
+
+	"github.com/claceio/clace/internal/app/apptype"
+	"github.com/go-chi/chi"
+	"go.starlark.net/starlark"
+)
+
+//go:embed *.go.html static/*
+var embedHtml embed.FS
+
+// Action represents a single action that is exposed by the App. Actions
+// provide a way to trigger app operations, with an auto-generated form UI
+// and an API interface
+type Action struct {
+	name        string
+	description string
+	path        string
+	run         starlark.Callable
+	validate    starlark.Callable
+	params      []apptype.AppParam
+	paramMap    map[string]string
+	template    *template.Template
+	pagePath    string
+}
+
+// NewAction creates a new action
+func NewAction(name, description, apath string, run, validate starlark.Callable,
+	params []apptype.AppParam, paramMap map[string]string, appPath string) (*Action, error) {
+	tmpl, err := template.New("form").ParseFS(embedHtml, "*.go.html")
+	if err != nil {
+		return nil, err
+	}
+
+	slices.SortFunc(params, func(a, b apptype.AppParam) int {
+		return a.Index - b.Index
+	})
+
+	return &Action{
+		name:        name,
+		description: description,
+		path:        apath,
+		run:         run,
+		validate:    validate,
+		params:      params,
+		paramMap:    paramMap,
+		template:    tmpl,
+		pagePath:    path.Join(appPath, apath),
+	}, nil
+}
+
+func (a *Action) BuildRouter() (*chi.Mux, error) {
+	fSys, err := fs.Sub(embedHtml, "static")
+	if err != nil {
+		return nil, err
+	}
+	staticServer := http.FileServer(http.FS(fSys))
+
+	r := chi.NewRouter()
+	r.Post("/", a.runHandler)
+	r.Get("/", a.getForm)
+	r.Handle("/static/*", http.StripPrefix(path.Join(a.pagePath, "/static/"), staticServer))
+	return r, nil
+}
+
+func (a *Action) runHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the form
+	// Validate the form
+	// Run the action
+}
+
+func (a *Action) getForm(w http.ResponseWriter, r *http.Request) {
+	input := map[string]any{
+		"name":        a.name,
+		"description": a.description,
+		"path":        a.pagePath,
+		"params":      a.params,
+		"paramValues": a.paramMap,
+	}
+	err := a.template.ExecuteTemplate(w, "form.go.html", input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
---
+++ internal/app/action/static/toggle.js
@@ -0,0 +1,24 @@
+// Check for saved theme on page load and update the toggle
+document.addEventListener('DOMContentLoaded', (event) => {
+    const theme = localStorage.getItem('theme');
+    const systemDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    toggle = document.getElementById('theme-toggle') 
+    if (theme == 'theme-dark' || (theme == null && systemDark)) {
+        toggle.checked = true;
+    } else {
+        toggle.checked = false;
+    }
+
+    // Update theme switch logic to save preference
+    toggle.addEventListener('change', function(event){
+        if(event.target.checked){
+            document.documentElement.setAttribute("data-theme", "dim")
+            localStorage.setItem('theme', 'theme-dark');
+        } else {
+            document.documentElement.setAttribute("data-theme", "lemonade")
+            localStorage.setItem('theme', 'theme-light');
+        }
+    });
+
+});
