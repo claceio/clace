48d5d5a3cfef13ad5e2c47c2df538b37f09c6a51
Ajay Kidave <ajayvk@clace.io>
2025-04-15 17:21:32 -0700

    Fix container test case

--- tests/flaskhttp/app.py
+++ tests/flaskhttp/app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 import configparser
 
-config = configparser.ConfigParser()
-config.read('/var/data.ini')
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,4 +9,9 @@ def root():
 
 @app.route("/test")
 def hello_world():
-    return config['DEFAULT']['PARAM1']
+    config = configparser.ConfigParser()
+    try:
+        config.read('/var/data.ini')
+    except Exception as e:
+        return str(e)
+    return config['DEFAULT']['PASSWD']
